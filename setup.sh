56c2184c542511023c89f02082ccf50adb376a07
Karl Ovrevik <karl@kovrevik.com>
2024-08-27 17:34:51 -0400

    needing to send files to another computer to test is a pain in the ass

--- setup.sh
+++ setup.sh
@@ -5,10 +5,15 @@ source ./hypr/scripts/install_everything.sh
 
 echo "Moving files to position"
 
+mkdir -p ~/.config/hypr
 cp -r ./hypr/* ~/.config/hypr
+mkdir -p ~/.config/kitty
 cp -r ./kitty/* ~/.config/kitty
+mkdir -p ~/.config/mako
 cp -r ./mako/* ~/.config/mako
+mkdir -p ~/.config/waybar
 cp -r ./waybar/* ~/.config/waybar
+mkdir -p ~/.config/wofi
 cp -r ./wofi/* ~/.config/wofi
 mkdir -p ~/Pictures/System/Backgrounds/Shikanokonokonokokoshitantan
 cp -r ./Images/* ~/Pictures/System/Backgrounds/Shikanokonokonokokoshitantan
